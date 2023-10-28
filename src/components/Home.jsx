7f1c9772359cebe6e7db8d29b69d9415ded7f44f
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2023-10-28 19:18:14 +0530

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,14 @@ import Switch, { switchClasses } from '@mui/material/Switch';
 import Stack from '@mui/material/Stack';
 import PauseIcon from '@mui/icons-material/Pause';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import { useEffect } from 'react';
 
 
 export default function Home() {
   var classes = useStyles();
   const token = useSelector(state => state.Token.Token);
   const [open, setOpen] = useState(false);
+  const [price,setPrice]=useState(0);
   const [checked, setChecked] = useState(true);
   const [value, setValue] = useState('Buy');
   const [play,setPlay]=useState(false);
@@ -20,11 +22,36 @@ export default function Home() {
     setOpen(true);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      let k=simulateStockPrice();
+      if(k!==price){
+        setPrice(k);
+      }
+      else{
+        setPrice(k=k+0.2);
+      }
+    }, 1000);
+  }, [price])
+
   const handleClose = () => {
     setOpen(false);
     setValue('Buy')
     setChecked(true)
   };
+
+  function simulateStockPrice() {
+    let price = 7;
+    const minChange = -0.1;
+    const maxChange = 0.1;
+    const priceChange = (Math.random() * (maxChange - minChange) + minChange).toFixed(2);
+    price += parseFloat(priceChange);
+    if (price < 1) {
+      price = 1;
+    }
+  
+    return price;
+  }
 
   const handleCheck = (event) => {
     setChecked(event.target.checked)
@@ -45,7 +72,7 @@ export default function Home() {
         </DialogTitle>
         <DialogContent>
           <Grid container xs={12}>
-            <Grid item xs={12}>
+            {/* <Grid item xs={12}>
               <Switch
                 checked={checked}
                 onChange={handleCheck}
@@ -57,7 +84,7 @@ export default function Home() {
                 }}
               />
               {value}
-            </Grid>
+            </Grid> */}
             <Grid item xs={6}>
               <TextField sx={{ color: "white" }} id="filled-password-input" label="Quantity" variant="standard" />
             </Grid>
@@ -81,10 +108,10 @@ export default function Home() {
       <Box className={classes.box}>
         <Box sx={{ display: 'flex', justifyContent: "space-around", width: "100%" }}>
           <Box sx={{ backgroundColor: "#2B2B2B", paddingX: "40px", paddingY: "10px", borderRadius: "20px" }}>
-            389
+            Current Price
           </Box>
           <Box sx={{ backgroundColor: "#2B2B2B", paddingX: "40px", paddingY: "10px", borderRadius: "20px" }}>
-            389
+            {price}
           </Box>
         </Box>
       </Box>
@@ -92,9 +119,9 @@ export default function Home() {
       <Box >
         <Box className={classes.box}>
           <Box className={classes.box1}>
-            <Button variant="contained" sx={{ paddingX: "40px", fontSize: "20px", fontWeight: "bolder" }} color="success" onClick={handleClickOpen}>BUY</Button>
+            <Button variant="contained" sx={{ paddingX: "40px", fontSize: "20px", fontWeight: "bolder" }} color="success">BUY</Button>
             {showForm()}
-            <Button variant="contained" sx={{ paddingX: "40px", fontSize: "20px", fontWeight: "bolder" }} color="error">SELL</Button>
+            <Button variant="contained" sx={{ paddingX: "40px", fontSize: "20px", fontWeight: "bolder" }} color="error" onClick={handleClickOpen}>SELL</Button>
           </Box>
         </Box>
         {/* overall */}
